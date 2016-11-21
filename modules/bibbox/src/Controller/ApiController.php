f08bf93c956571e11784e4c6ee8ec9e8ac91a6f5
Troels Ugilt Jensen <tujensen@gmail.com>
2016-11-21 12:20:01 +0100

    Issue #BIB-93: Fixed array merge with default

--- modules/bibbox/src/Controller/ApiController.php
+++ modules/bibbox/src/Controller/ApiController.php
@@ -12,11 +12,57 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Drupal\node\Entity\Node;
 use Drupal\file\Entity\File;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 /**
  * ApiController.
  */
 class ApiController extends ControllerBase {
+  /**
+   * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
+   * keys to arrays rather than overwriting the value in the first array with the duplicate
+   * value in the second array, as array_merge does. I.e., with array_merge_recursive,
+   * this happens (documented behavior):
+   *
+   * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
+   *     => array('key' => array('org value', 'new value'));
+   *
+   * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
+   * Matching keys' values in the second array overwrite those in the first array, as is the
+   * case with array_merge, i.e.:
+   *
+   * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
+   *     => array('key' => array('new value'));
+   *
+   * Parameters are passed by reference, though only for performance reasons. They're not
+   * altered by this function.
+   *
+   * @param array $array1
+   * @param array $array2
+   * @return array
+   * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
+   * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
+   *
+   * Modified to ignore null values in array2.
+   */
+  function array_merge_recursive_distinct_ignore_nulls(array &$array1, array &$array2) {
+    $merged = $array1;
+
+    foreach ($array2 as $key => &$value) {
+      if (is_array($value) && isset ($merged[$key]) && is_array($merged[$key])) {
+        $merged[$key] = $this->array_merge_recursive_distinct_ignore_nulls($merged[$key], $value);
+      }
+      else {
+        if (isset($value)) {
+          $merged[$key] = $value;
+        }
+      }
+    }
+
+    return $merged;
+  }
+
+
   /**
    * Push config to a machine.
    *
@@ -28,10 +74,34 @@ class ApiController extends ControllerBase {
     // Get destination to return to after completing request.
     $destination = $request->query->get('destination');
 
+    // Load node.
+    $node = \Drupal::entityManager()->getStorage('node')->load($id);
+
+    // Get machine array.
     $machine = $this->getMachineArray($id);
 
+    // If the machine exists.
     if ($machine) {
-      // @TODO: Do the stuff!
+      // Merge with Default if one exists.
+      if (count($node->get('field_default'))) {
+        // Get default array.
+        $default = $this->getMachineArray($node->get('field_default')[0]->getValue()['target_id']);
+
+        // Merge.
+        $machine = $this->array_merge_recursive_distinct_ignore_nulls($default, $machine);
+      }
+
+      // Get IP
+      $ip = $node->get('field_ip')->value;
+
+      // Make sure it is set.
+      if (!isset($ip)) {
+        throw new HttpException('IP not set');
+      }
+
+      // Send request.
+      $client = \Drupal::httpClient();
+      $client->request('POST', $node->get('field_ip')->value . "/api/config", array('json' => $machine));
     }
 
     return new RedirectResponse($destination);
@@ -50,28 +120,6 @@ class ApiController extends ControllerBase {
 
     // @TODO: Do the stuff!
 
-    return new RedirectResponse($destination);
-  }
-
-  /**
-   * Clone a machine node.
-   *
-   * @param \Symfony\Component\HttpFoundation\Request $request
-   * @param $id
-   * @return \Symfony\Component\HttpFoundation\RedirectResponse
-   */
-  public function cloneMachine(Request $request, $id) {
-    // Get destination to return to after completing request.
-    $destination = $request->query->get('destination');
-
-    $node = \Drupal::entityManager()->getStorage('node')->load($id);
-/*
-    $clone = Node::create([
-      'type'        => 'machine',
-      'title'       => '',
-    ]);
-    $clone->save();
-*/
     return new RedirectResponse($destination);
   }
 
@@ -101,17 +149,17 @@ class ApiController extends ControllerBase {
    * @param $nid
    *   Node id.
    *
-   * @return object
-   *   Machine represented as object.
+   * @return array
+   *   Machine represented as array.
    */
   private function getMachineArray($nid) {
     $node = \Drupal::entityManager()->getStorage('node')->load($nid);
 
     // Create machine object.
-    $machine = (object) [
+    $machine = [
       'title' => $node->get('title')->value,
       'email' => $node->get('field_email')->value,
-      'fbs' => (object)[
+      'fbs' => [
         'username' => $node->get('field_fbs_username')->value,
         'password' => $node->get('field_fbs_password')->value,
         'endpoint' => $node->get('field_fbs_endpoint')->value,
@@ -122,21 +170,21 @@ class ApiController extends ControllerBase {
           'timeLimit' => $node->get('field_fbs_login_attempts_time_li')->value,
         ],
       ],
-      'ui' => (object)[
-        'timeout' => (object)[
+      'ui' => [
+        'timeout' => [
           'idleTimeout' => $node->get('field_timeout')->value,
           'idleWarn' => $node->get('field_timeout_idle')->value,
         ],
-        'binSorting' => (object) [
+        'binSorting' => [
           'default_bin' => $node->get('field_bin_default')->value ? 'left' : 'right',
-          'destinations' => (object) [
-            'left' => (object) [
+          'destinations' => [
+            'left' => [
               'id' => 'left',
               'text' => $node->get('field_bin_left_text')->value,
               'background_color' => $node->get('field_bin_left_background_color')->value,
               'color' => $node->get('field_bin_left_color')->value,
             ],
-            'right' => (object) [
+            'right' => [
               'id' => 'right',
               'text' => $node->get('field_bin_right_text')->value,
               'background_color' => $node->get('field_bin_right_background_color')->value,
@@ -157,28 +205,28 @@ class ApiController extends ControllerBase {
         // Will be attached later.
         'languages' => [],
       ],
-      'notification' => (object) [
-        'config' => (object)[
+      'notification' => [
+        'config' => [
           'default_lang' => $node->get('field_notification_default_lang')->value,
           'date_format' => $node->get('field_notification_date_format')->value,
         ],
-        'mailer' => (object)[
+        'mailer' => [
           'host' => $node->get('field_notification_mailer_host')->value,
           'port' => $node->get('field_notification_mailer_port')->value,
           'secure' => $node->get('field_notification_mailer_secure')->value ? true : false,
           'from' => $node->get('field_notification_mailer_from')->value,
           'subject' => $node->get('field_notification_mailer_subjec')->value,
         ],
-        'header' => (object)[
+        'header' => [
           'brand' => $node->get('field_notification_header_brand')->value,
           'logo' => $node->get('field_notification_header_logo')->value,
           'color' => $node->get('field_notification_header_color')->value,
         ],
-        'footer' => (object)[
+        'footer' => [
           'html' => $node->get('field_notification_footer_html')->value,
           'text' => $node->get('field_notification_footer_text')->value,
         ],
-        'library' => (object)[
+        'library' => [
           'title' => $node->get('field_notification_library_title')->value,
           'name' => $node->get('field_notification_library_name')->value,
           'address' => $node->get('field_notification_library_addre')->value,
@@ -187,7 +235,7 @@ class ApiController extends ControllerBase {
           'phone' => $node->get('field_notification_library_phone')->value,
         ],
         // Will be attached later.
-        'layouts' => (object)[
+        'layouts' => [
           'status' => [],
           'checkIn' => [],
           'checkOut' => [],
@@ -198,31 +246,30 @@ class ApiController extends ControllerBase {
 
     // Attach notification layouts
     foreach ($node->get('field_notification_layouts_statu') as $item) {
-      $machine->notification->layouts->status[$item->value] = true;
+      $machine['notification']['layouts']['status'][$item->value] = true;
     }
     foreach ($node->get('field_notification_layouts_cout') as $item) {
-      $machine->notification->layouts->checkOut[$item->value] = true;
+      $machine['notification']['layouts']['checkOut'][$item->value] = true;
     }
     foreach ($node->get('field_notification_layouts_cin') as $item) {
-      $machine->notification->layouts->checkIn[$item->value] = true;
+      $machine['notification']['layouts']['checkIn'][$item->value] = true;
     }
     foreach ($node->get('field_notification_layouts_reser') as $item) {
-      $machine->notification->layouts->reservations[$item->value] = true;
+      $machine['notification']['layouts']['reservations'][$item->value] = true;
     }
 
     // Attach bins to binSorting.
     foreach ($node->get('field_bin_left') as $item) {
-      $machine->ui->binSorting->bins["" . $item->value] = 'left';
+      $machine['ui']['binSorting']['bins']["" . $item->value] = 'left';
     }
     foreach ($node->get('field_bin_right') as $item) {
-      $machine->ui->binSorting->bins["" . $item->value] = 'right';
+      $machine['ui']['binSorting']['bins']["" . $item->value] = 'right';
     }
-    $machine->ui->binSorting->bins = (object) $machine->ui->binSorting->bins;
 
     // Attach features.
     foreach ($node->get('field_features') as $ref) {
       $feature = \Drupal::entityManager()->getStorage('node')->load($ref->getValue()['target_id']);
-      $machine->ui->features[] = (object)[
+      $machine['ui']['features'][] = [
         'title' => $feature->get('title')->value,
         'icon' => $feature->get('field_icon')->value,
         'require_offline' => boolval($feature->get('field_require_online')->value),
@@ -234,7 +281,7 @@ class ApiController extends ControllerBase {
     // Attach languages.
     foreach ($node->get('field_languages') as $ref) {
       $language = \Drupal::entityManager()->getStorage('node')->load($ref->getValue()['target_id']);
-      $machine->ui->languages[] = (object)[
+      $machine['ui']['languages'][] = [
         'title' => $language->get('title')->value,
         'text' => $language->get('field_text')->value,
         'langKey' => $language->get('field_language_key')->value,
