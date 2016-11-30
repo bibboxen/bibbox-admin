9e80fe9020b87058861ec9e2ea17314ca22e4b1a
Troels Ugilt Jensen <tujensen@gmail.com>
2016-11-30 12:11:46 +0100

    Issue #BIB-182: Merged develop

---
+++ web/modules/custom/translation_field/src/Plugin/Field/FieldType/TranslationItem.php
@@ -0,0 +1,121 @@
+<?php
+/**
+* @file
+* Contains Drupal\translation_field\Plugin\Field\FieldType\TranslationItem.
+*/
+
+namespace Drupal\translation_field\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
+use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
+
+/**
+ * Provides a field type of a translation
+ *
+ * @FieldType(
+ *   id = "translation",
+ *   label = @Translation("Translation"),
+ *   module = "translation_field",
+ *   description = @Translation("Provides a translation field"),
+ *   default_widget = "field_translation_widget",
+ *   default_formatter = "field_translation_formatter"
+ * )
+ */
+class TranslationItem extends EntityReferenceItem {
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    return array(
+      'columns' => array(
+        'key' => array(
+          'type' => 'text',
+          'size' => 'tiny',
+          'not null' => TRUE,
+        ),
+        'value' => array(
+          'type' => 'text',
+          'size' => 'tiny',
+          'not null' => TRUE,
+        ),
+        'target_id' => array(
+          'description' => 'The ID of the node.',
+          'type' => 'int',
+          'unsigned' => TRUE,
+          'not null' => FALSE,
+        ),
+      ),
+      'indexes' => array(
+        'target_id' => array('target_id'),
+      ),
+      'foreign keys' => array(
+        'target_id' => array(
+          'table' => 'node',
+          'columns' => array('target_id' => 'nid'),
+        ),
+      ),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEmpty() {
+    $value1 = $this->get('key')->getValue();
+    $value2 = $this->get('value')->getValue();
+    $value3 = $this->get('target_id')->getValue();
+    return empty($value1) && empty($value2) && empty($value3);
+  }
+
+
+  /**
+   * Determines whether the item holds an unsaved entity.
+   *
+   * This is notably used for "autocreate" widgets, and more generally to
+   * support referencing freshly created entities (they will get saved
+   * automatically as the hosting entity gets saved).
+   *
+   * @return bool
+   *   TRUE if the item holds an unsaved entity.
+   */
+  public function hasNewEntity() {
+    return !$this->isEmpty() && $this->target_id === NULL && isset($this->entity) && $this->entity->isNew();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preSave() {
+    if ($this->hasNewEntity()) {
+      // Save the entity if it has not already been saved by some other code.
+      if ($this->entity->isNew()) {
+        $this->entity->save();
+      }
+      // Make sure the parent knows we are updating this property so it can
+      // react properly.
+      $this->target_id = $this->entity->id();
+    }
+    if (!$this->isEmpty() && $this->target_id === NULL && isset($this->entity)) {
+      $this->target_id = $this->entity->id();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties = parent::propertyDefinitions($field_definition);
+
+    $properties['key'] = DataDefinition::create('string')
+      ->setLabel(t('Key'))
+      ->setDescription(t('The key of the translation'));
+    $properties['value'] = DataDefinition::create('string')
+      ->setLabel(t('Value'))
+      ->setDescription(t('The translation'));
+
+    $properties['target_id']->setRequired(FALSE);
+
+    return $properties;
+  }
+}
---
+++ web/modules/custom/translation_field/src/Plugin/Field/FieldWidget/TranslationWidget.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\translation_field\Plugin\Field\FieldWidget\TranslationWidget.
+ */
+
+namespace Drupal\translation_field\Plugin\Field\FieldWidget;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\WidgetBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'field_translation' widget.
+ *
+ * @FieldWidget(
+ *   id = "field_translation_widget",
+ *   module = "translation_field",
+ *   label = @Translation("Translation"),
+ *   field_types = {
+ *     "field_translation"
+ *   }
+ * )
+ */
+class TranslationWidget extends WidgetBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $element['#prefix'] = '<div class="translation-field-wrapper">';
+    $element['#suffix'] = '</div>';
+
+    $element['key'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Key'),
+      '#default_value' => isset($items[$delta]->key) ? $items[$delta]->key : '',
+      '#size' => 25,
+      '#attributes' => array('class' => array('translation-field-key')),
+    );
+    $element['value'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Value'),
+      '#default_value' => isset($items[$delta]->value) ? $items[$delta]->value : '',
+      '#size' => 25,
+      '#attributes' => array('class' => array('translation-field-value')),
+    );
+    $element['target_id'] = array(
+      '#type' => 'entity_autocomplete',
+      '#title' => t('Machine'),
+      '#target_type' => 'node',
+      '#default_value' => isset($items[$delta]->entity) ? $items[$delta]->entity : null,
+      '#selection_settings' => [
+        'target_bundles' => ['machine'],
+      ],
+      '#size' => 25,
+      '#weight'   => 5,
+      '#settings' => array(
+        'match_operator'    => 'CONTAINS',
+        'size'              => '60',
+        'autocomplete_type' => 'node',
+      ),
+      '#required' => FALSE,
+  );
+
+    // If cardinality is 1, ensure a label is output for the field by wrapping
+    // it in a details element.
+    if ($this->fieldDefinition->getFieldStorageDefinition()->getCardinality() == 1) {
+      $element += array(
+        '#type' => 'fieldset',
+        '#attributes' => array('class' => array('container-inline')),
+      );
+    }
+
+    return $element;
+  }
+
+    /**
+   * Validate the color text field.
+   */
+  public function validate($element, FormStateInterface $form_state) {
+    $value = $element['#value'];
+    if (strlen($value) == 0) {
+      $form_state->setValueForElement($element, '');
+      return;
+    }
+    if (!preg_match('/^#([a-f0-9]{6})$/iD', strtolower($value))) {
+      $form_state->setError($element, t("Color must be a 6-digit hexadecimal value, suitable for CSS."));
+    }
+  }
+
+}
