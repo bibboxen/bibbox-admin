2728d562c33bd57b2126e8ec30f4c8e9ad0888bf
Troels Ugilt Jensen <tujensen@gmail.com>
2017-06-12 19:20:50 +0200

    Issue #BIB-235: Started implementing bulk operations

---
+++ web/modules/custom/bibbox/src/Plugin/Action/PushTranslation.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @file
+ * Contains the push translation action.
+ */
+
+namespace Drupal\bibbox\Plugin\Action;
+
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Push translation to machine.
+ *
+ * @Action(
+ *   id = "push_translation",
+ *   label = @Translation("Push translation to machine"),
+ *   type = "node"
+ * )
+ */
+class PushTranslation extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    if (!is_null($entity)) {
+      \Drupal::service('bibbox.proxy')->pushTranslation($entity->id());
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    /** @var \Drupal\taxonomy\TermInterface $object */
+    $result = $object->access('update', $account, TRUE);
+
+    return $return_as_object ? $result : $result->isAllowed();
+  }
+
+}
