93ca3e98c22275619b477b4cadc4bb1b5d060426
Jesper Kristensen <cableman@linuxdev.dk>
2020-06-15 17:10:31 +0200

    BIB-366: Added screen on/off fields

---
+++ web/themes/custom/bibbox_theme/js/bibbox.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Add time picker to screen on/off fields.
+ */
+(function ($, Drupal, drupalSettings) {
+
+  'use strict';
+
+  Drupal.behaviors.mybehavior = {
+    attach: function (context, settings) {
+      $('#edit-field-screen-on-0-value').timepicker({ 'timeFormat': 'H:i:s' });
+      $('#edit-field-screen-o-0-value').timepicker({ 'timeFormat': 'H:i:s' });
+    }
+  };
+
+})(jQuery, Drupal, drupalSettings);
